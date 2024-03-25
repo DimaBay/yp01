bb4e4b0e0d24d30b11dd3a533b893cf3f49b661b
Dima <dironthebest@gmail.com>
2024-03-26 03:37:37 +0500

    Дизайн страниц входа

--- app/src/main/java/com/example/yp01/MainActivity.java
+++ app/src/main/java/com/example/yp01/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.yp01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.launchscreen);
+
+        progressBar = findViewById(R.id.progressBar);
+
+
+        if (isNetworkAvailable()) {
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+        }
     }
+    public void onStepScreen1(View view)
+    {
+        setContentView(R.layout.onboardingscreen);
+    }
+    public void onStepScreen2(View view)
+    {
+        setContentView(R.layout.onboadringscreen2);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
 }
